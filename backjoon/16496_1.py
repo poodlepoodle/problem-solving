978b69a6faa708dac2af1a83d43b61bb828e6110
poodlepoodle <chammal97@naver.com>
2022-11-24 14:42:33 +0900

    :sparkles: 16496번: 큰 수 만들기 풀이
    1번째 풀이 (python)

--- backjoon/16496_1.py
+++ backjoon/16496_1.py
@@ -10,25 +10,20 @@ def compare(str1, str2):
     i, j = 0, 0
 
     while True:
-        if str1[i] > str2[j]:
+        if str1[i % len(str1)] > str2[j % len(str2)]:
             return 1
-        elif str1[i] < str2[j]:
+        elif str1[i % len(str1)] < str2[j % len(str2)]:
             return -1
 
-        if i == len(str1) - 1 and j == len(str2) - 1: break
+        if i >= 2 * len(str1) - 1 and j >= 2 * len(str2) - 1: break
         
-        if i < len(str1) - 1:
-            i += 1
-        if j < len(str2) - 1:
-            j += 1
+        i += 1
+        j += 1
     
-    # 길이가 같을 경우는 더 긴 수 출력
-    if len(str1) != len(str2):
-        return 1 if len(str1) > len(str2) else -1
-    return 0
+    # return 0
 
-for i in range(len(numbers), 1, -1):
-    for j in range(i - 1):
+for i in range(len(numbers)):
+    for j in range(0, len(numbers) - i - 1):
         if compare(numbers[j], numbers[j + 1]) < 1:
             numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
